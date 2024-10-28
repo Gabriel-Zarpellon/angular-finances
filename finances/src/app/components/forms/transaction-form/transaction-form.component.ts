7cb64cbb4d067195d9bff7ec1a87029fa12c66fd
Gabriel Zarpellon <gabriel_zv@hotmail.com>
2024-10-28 12:58:01 -0300

    feat: implement formGroup

--- finances/src/app/components/forms/transaction-form/transaction-form.component.ts
+++ finances/src/app/components/forms/transaction-form/transaction-form.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { TransactionService } from '../../../services/transaction.service';
 import { tCreateTransaction } from '../../../interfaces/transaction.interface';
 
@@ -13,22 +13,36 @@ import { tCreateTransaction } from '../../../interfaces/transaction.interface';
 export class TransactionFormComponent {
   constructor(private transactionService: TransactionService) {}
 
-  amount = new FormControl<number>(0);
-  type = new FormControl<'deposit' | 'withdraw'>('deposit');
-  description = new FormControl<string>('');
+  transactionForm = new FormGroup({
+    amount: new FormControl<number>(0),
+    type: new FormControl<'deposit' | 'withdraw'>('deposit', {
+      nonNullable: true,
+    }),
+    description: new FormControl<string>(''),
+  });
 
-  formSubmit(event: Event) {
-    event.preventDefault();
-    let formData = {
-      amount: this.amount.value,
-      type: this.type.value,
-      description: this.description.value,
-    };
-
+  onSubmit() {
+    let formData = this.transactionForm.value;
     this.transactionService.addTransaction(formData as tCreateTransaction);
-
-    this.amount.setValue(0);
-    this.type.setValue('deposit');
-    this.description.setValue('');
+    this.transactionForm.reset();
   }
+
+  // amount = new FormControl<number>(0);
+  // type = new FormControl<'deposit' | 'withdraw'>('deposit');
+  // description = new FormControl<string>('');
+
+  // formSubmit(event: Event) {
+  //   event.preventDefault();
+  //   let formData = {
+  //     amount: this.amount.value,
+  //     type: this.type.value,
+  //     description: this.description.value,
+  //   };
+
+  //   this.transactionService.addTransaction(formData as tCreateTransaction);
+
+  //   this.amount.setValue(0);
+  //   this.type.setValue('deposit');
+  //   this.description.setValue('');
+  // }
 }
